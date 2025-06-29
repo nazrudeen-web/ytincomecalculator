341b79cc6c4ce47d19b802202c6349907e6113d7
nazrudeen <nazrudeen.win@gmail.com>
2025-06-29 22:17:15 +0400

    bug new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-<<<<<<< HEAD
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
@@ -26,24 +25,12 @@ const dynamicChannels = [
   'elrubiusomg-net-worth',
   'ed-sheeran-net-worth',
 ];
-=======
-import tailwindcss from "@tailwindcss/vite";
-import sitemap from "@astrojs/sitemap";
-import dynamicPages from "./sitemap.config.mjs";
->>>>>>> 0498ca7c1be66d301c862608b197ec5338bef390
 
-export default defineConfig(async () => ({
+export default defineConfig({
   output: "server",
-  site: "https://ytincomecalculator.pages.dev/",
-  integrations: [
-    sitemap({
-      customPages: await dynamicPages(),
-    }),
-  ],
   adapter: cloudflare({
     imageService: "cloudflare",
   }),
-<<<<<<< HEAD
   site: 'https://ytincomecalculator.pages.dev',
   integrations: [
     sitemap({
@@ -54,9 +41,3 @@ export default defineConfig(async () => ({
     tailwind(),
   ],
 });
-=======
-  vite: {
-    plugins: [tailwindcss()],
-  },
-}));
->>>>>>> 0498ca7c1be66d301c862608b197ec5338bef390
