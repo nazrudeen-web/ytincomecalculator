df0cfe2dc166b24083e0ca976b478620b11864e6
nazrudeen <nazrudeen.win@gmail.com>
2025-07-02 21:24:48 +0400

    improving home page ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         poppins: ['Poppins', 'sans-serif'], // add your custom name
         sora: ['Sora', 'sans-serif'], // add your custom name
       },
+      colors: {
+        accent: '#ff0000',
+        bcPrimary: '#fff',
+        bcSecondary: '#F9F9F9',
+        tPrimary: '#0F0F0F',
+        tSecondary: '#606060',
+        divider: '#E5E5E5',
+        bTag: '#F2F2F2',
+      }
     },
   },
   plugins: [require('@tailwindcss/typography')],
